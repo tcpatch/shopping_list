1a784c1bcfb5b7fdcc64d3380da535925df0849a
tcpatch <tcpatch2014@gmail.com>
2020-02-18 02:30:49 +0000

    basic functioning recipe object

--- recipe.py
+++ recipe.py
@@ -18,43 +18,79 @@ class Recipe:
         self.sub_recipe = None
         self.tags = None
 
-        @property
-        def ingredients(self):
-            return self._ingredients
+    @property
+    def ingredients(self):
+        return self._ingredients
 
-        @ingredients.setter
-        def ingredients(self, ingredients):
-            for i in ingredients:
-                if i not in self.all_ingredients:
-                    self.all_ingredients.add(i)
+    @ingredients.setter
+    def ingredients(self, ingredients):
+        for i in ingredients:
+            if i not in self.all_ingredients:
+                self.all_ingredients.add(i)
             self._ingredients = ingredients
-        
-        def load_from_file(self, fp):
-            with open(fp, 'r') as f:
-                lines = f.readlines()
-            #TODO change to all html like tags and parse that way so it isnt hard coded
-            self.name = lines[0].strip()
-            self.prep_time = lines[1]
-            self.cook_time = lines[2]
-            self.total_time = self.prep_time + self.total_time
-#TODO parse file
-#TODO make a function to parse text between html tags and return that
 
-        def parse_tags(self):
-            tag_dict = dict()
-            for t in self.tags:
-                tag_path = t.split('/')
-                path_length = len(tag_path)
-                current_dict = tag_dict
-                for i in range(0, path_length):
-                    t = tag_path[i]
-                    if t not in current_dict:
-                        if i + 1 == path_length:
-                            current_dict[t] = tag_path[i+1]
-                            break
-                        else:
-                            current_dict[t] = dict() #todo finish up logic
-                            current_dict = current_dict[t]
+    @staticmethod
+    def as_type(i, _type):
+        if _type == 'str':
+            ret = str(i)
+        elif _type == 'int':
+            ret = int(i)
+        elif _type == 'float':
+            ret = float(i)
+        elif _type == 'bool':
+            ret = bool(i)
+        elif _type == 'long':
+            ret = long(i)
+        else:
+            ret = i
+        return ret
+
+    def find_tag(self, lines, tag, _type=None):
+        start_tag = '<' + tag + '>'
+        end_tag = '</' + tag + '>'
+        start = lines.index(start_tag) + 1
+        end = lines.index(end_tag)
+        contents = lines[start:end]
+        if len(contents) == 1:
+            contents = contents[0]
+        if _type:
+            contents = self.as_type(contents, _type)
+        return contents
+        
+    def load_from_file(self, fp):
+        with open(fp, 'r') as f:
+            lines = f.readlines()
+        lines = [i.strip() for i in lines]
+        tags = [('name', 'str'), 
+                ('prep_time', 'int'),
+                ('cook_time', 'int'),
+                ('cook_temp', 'int'),
+                ('ingredients', None),
+                ('recipe', None),
+                ('tags', None)]
+        for t in tags:
+            if t[0] == 'ingredients':
+                setattr(self, '_ingredients', self.find_tag(lines, t[0], t[1]))
+            else:
+                setattr(self, t[0], self.find_tag(lines, t[0], t[1]))
+        self.total_time = self.prep_time + self.cook_time
+
+    def parse_tags(self):
+        tag_dict = dict()
+        for tag in self.tags:
+            tag_path = tag.split('/')
+            path_length = len(tag_path) - 1
+            current_dict = tag_dict
+            for i in range(0, path_length):
+                t = tag_path[i]
+                if t not in current_dict:
+                    if i + 1 == path_length:
+                        current_dict[t] = tag_path[i+1]
+                        break
                     else:
-                        current_dict = current_dict[t] #TODO deal with if end of another tag in dictionary...
+                        current_dict[t] = dict() #todo finish up logic
+                        current_dict = current_dict[t]
+                else:
+                    current_dict = current_dict[t] #TODO deal with if end of another tag in dictionary...
+        print(tag_dict)
 
