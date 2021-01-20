16f730293cb08ca44a2bd131673c32b1c188b4b6
tcpatch <tcpatch@github.com>
2021-01-20 15:28:50 -0500

    working prototype and added some more recipes

---
+++ settings.py
@@ -0,0 +1,2 @@
+user_staples = [['eggs', 1], ['milk', 1], ['english muffins', 1], ['water', 6]]
+user_exclude_list = ['salt', 'salt & pepper', 'salt and pepper', 'butter', 'flour', 'milk', 'olive oil']
